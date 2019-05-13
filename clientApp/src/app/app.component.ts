96a7cc8ac6bdcbac7660b05d113c7c91ba50f685
Oscar Pulido Rodriguez <oscar.pulido95@gmail.com>
2019-05-13 15:10:19 -0500

    fix: responsive design added

--- clientApp/src/app/app.component.ts
+++ clientApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'clientApp';
+
+  sidebarOpened: boolean = false;
+
+  constructor(private renderer: Renderer2) { }
+
+  toggleMenu() {
+    this.sidebarOpened = !this.sidebarOpened;
+    this.sidebarOpened ? this.renderer.setStyle(document.body, 'overflow', 'hidden') : this.renderer.setStyle(document.body, 'overflow', 'auto');
+  }
 }
